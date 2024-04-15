a60c40fb7fc87ef098140e7822539e11e038501e
Daten10 <tendanil563@gmail.com>
2024-04-15 20:46:37 +0600

    реализация работы с бд

---
+++ db/queries.py
@@ -0,0 +1,12 @@
+class Queries:
+    CREATE_REVIEW_TABLE = """
+        CREATE TABLE IF NOT EXISTS review (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            phone INTEGER,
+            date TEXT,
+            rate TEXT,
+            clean TEXT,
+            comment TEXT
+        )
+    """
