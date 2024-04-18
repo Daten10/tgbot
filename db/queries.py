de68773b7d932e2511df22f2b6a3b543a8864e44
Daten10 <tendanil563@gmail.com>
2024-04-18 19:30:58 +0600

    сделал запрос блюд по названию категории

--- db/queries.py
+++ db/queries.py
@@ -10,3 +10,35 @@ class Queries:
             comment TEXT
         )
     """
+
+    CREATE_CATEGORIES_TABLE = """
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT
+            )
+        """
+
+    CREATE_DISHES_TABLE = """
+            CREATE TABLE IF NOT EXISTS dishes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                price INTEGER,
+                picture TEXT,
+                category_id INTEGER,
+                FOREIGN KEY (category_id) REFERENCES categories(id)
+            )
+        """
+
+    DROP_CATEGORIES_TABLE = "DROP TABLE IF EXISTS categories"
+    DROP_DISHES_TABLE = "DROP TABLE IF EXISTS dishes"
+
+    POPULATE_CATEGORIES = """
+    INSERT INTO categories (name) VALUES ('пиццы'), ('горячие блюда'), ('гарниры'), ('напитки') 
+    """
+
+    POPULATE_DISHES = """
+    INSERT INTO dishes (name, price, picture, category_id) VALUES ('Куринная пицца', '465', 'pics/Medium.png', '1'),
+    ('Китайский суп', '380', 'pics/soup.png', '2'),
+    ('Картошка фри', '180', 'pics/fry.png', '3'),
+    ('Лимонад', '120', 'pics/juice.png', '4')
+    """
--- handlers/menu.py
+++ handlers/menu.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F, types
 from aiogram.filters import Command
-
+from config import database
 
 menu_router = Router()
 
@@ -22,59 +22,98 @@ async def shop(message: types.Message):
     await message.answer('Меню:', reply_markup=kb)
 
 
+category = ['пиццы', 'горячие блюда', 'гарниры', 'напитки']
+
+
+@menu_router.message(F.text.lower().in_(category))
+async def show_horror(message: types.Message):
+    categ = message.text.lower()
+    print(categ)
+    kb = types.ReplyKeyboardRemove()
+    data = await database.fetch(
+        """
+        SELECT dishes.* FROM dishes 
+        JOIN categories ON dishes.category_id = categories.id
+        WHERE categories.name = ?
+        """,
+        (categ,),
+        fetch_type='all'
+    )
+    if not data:
+        await message.answer('По вашему запросу ничего не найдено', reply_markup=kb)
+    await message.answer(f'Все наши блюда категории {categ}:')
+    for dish in data:
+        price = dish['price']
+        name = dish['name']
+        photo = types.FSInputFile(dish['picture'])
+        keyboard = types.InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    types.InlineKeyboardButton(text='Заказать', callback_data='order_'),
+                ]
+            ]
+        )
+        await message.answer_photo(
+            photo=photo,
+            caption=f'Название блюда: {name}\nЦена: {price} сом',
+            reply_markup=keyboard
+
+        )
+
+
 @menu_router.callback_query(F.data.startswith('order_'))
 async def shop_order(callback: types.CallbackQuery):
     await callback.answer('Добавлено в корзину!')
 
 
-@menu_router.message(F.text.lower() == 'пиццы')
-async def pizza_order(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                types.InlineKeyboardButton(text='Заказать', callback_data='order_1'),
-            ]
-        ]
-    )
-    file = types.FSInputFile("D:/pyProjects/pythonProject/pics/Medium.png")
-    await message.answer_photo(photo=file, caption='Пицца куриная 35см', reply_markup=keyboard)
-
-
-@menu_router.message(F.text.lower() == 'гарниры')
-async def pizza_order(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                types.InlineKeyboardButton(text='Заказать', callback_data='order_2'),
-            ]
-        ]
-    )
-    file = types.FSInputFile("D:/pyProjects/pythonProject/pics/fry.png")
-    await message.answer_photo(photo=file, caption='Картошка фри', reply_markup=keyboard)
-
-
-@menu_router.message(F.text.lower() == 'горячие блюда')
-async def pizza_order(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                types.InlineKeyboardButton(text='Заказать', callback_data='order_3'),
-            ]
-        ]
-    )
-    file = types.FSInputFile("D:/pyProjects/pythonProject/pics/soup.png")
-    await message.answer_photo(photo=file, caption='Китайский суп', reply_markup=keyboard)
-
-
-@menu_router.message(F.text.lower() == 'напитки')
-async def pizza_order(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                types.InlineKeyboardButton(text='Заказать', callback_data='order_4'),
-            ]
-        ]
-    )
-    file = types.FSInputFile("D:/pyProjects/pythonProject/pics/juice.png")
-    await message.answer_photo(photo=file, caption='Лимонад', reply_markup=keyboard)
-
+# @menu_router.message(F.text.lower() == 'пиццы')
+# async def pizza_order(message: types.Message):
+#     keyboard = types.InlineKeyboardMarkup(
+#         inline_keyboard=[
+#             [
+#                 types.InlineKeyboardButton(text='Заказать', callback_data='order_1'),
+#             ]
+#         ]
+#     )
+#     file = types.FSInputFile("D:/pyProjects/pythonProject/pics/Medium.png")
+#     await message.answer_photo(photo=file, caption='Пицца куриная 35см', reply_markup=keyboard)
+#
+#
+# @menu_router.message(F.text.lower() == 'гарниры')
+# async def pizza_order(message: types.Message):
+#     keyboard = types.InlineKeyboardMarkup(
+#         inline_keyboard=[
+#             [
+#                 types.InlineKeyboardButton(text='Заказать', callback_data='order_2'),
+#             ]
+#         ]
+#     )
+#     file = types.FSInputFile("D:/pyProjects/pythonProject/pics/fry.png")
+#     await message.answer_photo(photo=file, caption='Картошка фри', reply_markup=keyboard)
+#
+#
+# @menu_router.message(F.text.lower() == 'горячие блюда')
+# async def pizza_order(message: types.Message):
+#     keyboard = types.InlineKeyboardMarkup(
+#         inline_keyboard=[
+#             [
+#                 types.InlineKeyboardButton(text='Заказать', callback_data='order_3'),
+#             ]
+#         ]
+#     )
+#     file = types.FSInputFile("D:/pyProjects/pythonProject/pics/soup.png")
+#     await message.answer_photo(photo=file, caption='Китайский суп', reply_markup=keyboard)
+#
+#
+# @menu_router.message(F.text.lower() == 'напитки')
+# async def pizza_order(message: types.Message):
+#     keyboard = types.InlineKeyboardMarkup(
+#         inline_keyboard=[
+#             [
+#                 types.InlineKeyboardButton(text='Заказать', callback_data='order_4'),
+#             ]
+#         ]
+#     )
+#     file = types.FSInputFile("D:/pyProjects/pythonProject/pics/juice.png")
+#     await message.answer_photo(photo=file, caption='Лимонад', reply_markup=keyboard)
+#
